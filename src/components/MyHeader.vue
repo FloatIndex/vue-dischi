af3e791b2734452ccf64c402b1bbb211b237901c
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-23 17:44:11 +0100

    initial commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <img src="" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader'
+}
+</script>
+
+<style lang="scss" scope>
+header {
+    height: 60px;
+    background-color: slategrey;
+}
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">ciao</div>
+                <div class="col">ciao</div>
+                <div class="col">ciao</div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'MyMain'
+}
+</script>
+
+<style lang="scss" scope>
+main {
+    height: calc(100vh - 60px);
+    background-color: navy;
+    color: #fff;
+}
+</style>
