3fa0c6795b31203eff8d7314c0bd8e08a6802275
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-03-04 10:09:42 +0100

    push

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,26 +1,44 @@
 <template>
   <header>
       <img src="../assets/mtv-logo.png" alt="">
+      <SelectGenre : @change="makeResearch"/>
   </header>
 </template>
 
 <script>
+import SelectGenre from './partials/SelectGenre.vue';
+
 export default {
-    name: 'MyHeader'
+    name: 'MyHeader',
+    props: {
+        genres: Array
+    },
+    components: {
+        SelectGenre
+    },
+    data() {
+        return {
+            selectGenres: []
+        }
+    },
+    methods: {
+    }
 }
 </script>
 
-<style lang="scss" scope>
+<style lang="scss" scoped>
 @import '../style/variables.scss';
 header {
     height: 60px;
     background-color: $blue;
     display: flex;
+    justify-content: space-between;
     align-items: center;
+    padding: 0px 20px;
+
 
     img {
         width: 60px;
-        margin-left: 20px
     }
 }
 </style>
